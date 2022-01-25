58255fc9a4ca067c4ae845fb4f24204b0b1e1cd9
Armaan-Raisinghani <armaan5740@ggn.hxls.org>
2022-01-25 16:49:22 +0530

    e

--- events/client/ready.js
+++ events/client/ready.js
@@ -1,25 +1,3 @@
 module.exports = (Discord, client) => {
 console.log('Im ready')
-const activities = [{
-    name: 'with Chico',
-    type: 'PLAYING'
-}, {
-    name: 'giveaways',
-    type: 'WATCHING'
-},{
-    name:'/botinfo',
-    type:'PLAYING'
-}]
-
-client.user.setPresence({
-    activities: [activities[Math.floor(Math.random() * activities.length)]],
-    status: 'online'
-});
-
-setInterval(() => {
-    client.user.setPresence({
-        activities: [activities[Math.floor(Math.random() * activities.length)]],
-        status: 'online'
-    });
-}, 300000)
 }
