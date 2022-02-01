45c451062024c853dbadba329bed4484f2aeda0b
Neo <78310932+Krekensis@users.noreply.github.com>
2022-02-01 12:08:19 +0530

    why didnt this exist

--- commands/Utility/ping.js
+++ commands/Utility/ping.js
@@ -1,1 +1,16 @@
-
+const Discord = require('discord.js')
+module.exports = {
+    name: 'ping',
+    aliases: ['pong'],
+    async execute(message, args, cmd, client) {
+        const pingingEmbed = new Discord.MessageEmbed()
+        .setColor('BLURPLE')
+        .setDescription(`**Calculating ping...**`)
+        .setFooter('Slow lol?')
+        message.reply({embeds: [pingingEmbed]}).then((resultMessage) => {
+        const pingEmbed = new Discord.MessageEmbed()
+        .setColor('BLURPLE')
+        .setDescription(`**My ping is** \n\`\`\`yaml\n${client.ws.ping} ms\`\`\``)
+            resultMessage.edit({embeds: [pingEmbed]})
+        })
+    }
