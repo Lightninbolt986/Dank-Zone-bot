939d3458b957d3ccd2d14c940cd110b51d4dbab3
Neo <78310932+Krekensis@users.noreply.github.com>
2022-03-08 11:03:51 +0530

    Update add.js

--- commands/Donation/add.js
+++ commands/Donation/add.js
@@ -2,6 +2,18 @@ const emotes = require("../../data/emotes.json")
 const Discord = require('discord.js')
 const profileModel = require(`../../models/profileSchema`)
 
+const one = '767026839440326698' 
+const five = '772005506520186880' 
+const ten = '767026844221702154' 
+const twofive = '772004082277285918' 
+const fifty = '772004100953866263'
+const hundred = '779268054953492503'
+const twofifty = '779268051123830784'
+const fivehundred = '772005497762218024'
+const sevenfifty '798558270104010752'
+const billion = '772005505278935050'
+const twobillion = '815507562107633684' 
+
 module.exports = {
     name: 'add',
     aliases: ['note', 'dnote', 'dn'],
@@ -37,7 +49,7 @@ module.exports = {
             return message.reply(`${emotes.cross} Thats not a valid value.\n**Syntax:** \`${process.env.prefix}dnote <user> <amount>\``)
         }
 
-        //replace m => 1,000,000
+                //replace m => 1,000,000
         if (value.toLowerCase().includes("m")) {
             val = value.toLowerCase().replace("m", "")
             amount = parseInt(val * 1000000)
@@ -56,6 +68,7 @@ module.exports = {
         } else if (value.toLowerCase().includes("bil")) {
             val = value.toLowerCase().replace("bil", "")
             amount = parseInt(val * 1000000000)
+            
             //replace k => 1,000
         } else if (value.toLowerCase().includes("k")) {
             val = value.toLowerCase().replace("k", "")
@@ -63,7 +76,7 @@ module.exports = {
 
             //if args[1] doesnt have m, mil, k
         } else {
-            amount = parseInt(value)
+            amount = Number(value) || parseInt(value)
         }
 
         //if no value or not integer
@@ -97,7 +110,31 @@ module.exports = {
                 repliedUser: false
             }
         })
+        
+        const allroles = [one, five, ten, twofive, fifty, hundred, twofifty, fivehundred, sevenfifty, billion, twobillion]
+        let roles
+        if (response.dono >= 2500000000) { roles = [one, five, ten, twofive, fifty, hundred, twofifty, fivehundred, sevenfifty, billion, twobillion] }
+        else if (response.dono >= 1000000000 && response.dono < 2500000000) { roles = [one, five, ten, twofive, fifty, hundred, twofifty, fivehundred, sevenfifty, billion] }
+        else if (response.dono >= 750000000 && response.dono < 1000000000) { roles = [one, five, ten, twofive, fifty, hundred, twofifty, fivehundred, sevenfifty] }
+        else if (response.dono >= 500000000 && response.dono < 750000000) { roles = [one, five, ten, twofive, fifty, hundred, twofifty, fivehundred] }
+        else if (response.dono >= 250000000 && response.dono < 500000000) { roles = [one, five, ten, twofive, fifty, hundred, twofifty] }
+        else if (response.dono >= 100000000 && response.dono < 250000000) { roles = [one, five, ten, twofive, fifty, hundred] }
+        else if (response.dono >= 50000000 && response.dono < 100000000) { roles = [one, five, ten, twofive, fifty] }
+        else if (response.dono >= 25000000 && response.dono < 50000000) { roles = [one, five, ten, twofive] }
+        else if (response.dono >= 10000000 && response.dono < 25000000) { roles = [one, five, ten] }
+        else if (response.dono >= 5000000 && response.dono < 10000000) { roles = [one, five] }
+        else if (response.dono >= 1000000 && response.dono < 50000000) { roles = [one] }
+        else if (response.dono < 1000000) { roles = "none" }
 
+        allroles.forEach(r => {
+            user.roles.remove(r).catch(() => {})
+        })
+        if (roles !== "none") {
+            roles.forEach(r => {
+                user.roles.add(r).catch(() => {})
+            })
+        }
+        
         //log donation
         donationlog.send({
             embeds: [new Discord.MessageEmbed()
